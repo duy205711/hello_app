10c2a1cd04cba3c9e8402ceeb41a11bb7133aa4c
Lieu_Nguyen <rione94@gmail.com>
2017-03-27 16:47:01 +0700

    refactor style code by setting subl

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  	get  '/signup',  to: 'users#new'
-	root 'homes#index'  
+  get  '/signup',  to: 'users#new'
+  root 'homes#index'
 end
