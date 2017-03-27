0fcf5b9e27570b5ab7bc475707ae28fa1e3b8731
Lieu_Nguyen <rione94@gmail.com>
2017-03-27 18:27:43 +0700

    fix code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+    root 'home#index'
   resources :users
-  resources :homes, only: [:index]
+  resources :home, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'homes#index'
 end
