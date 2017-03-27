b7f1d30fed3f5360b421edb20c5a69f146e76282
Lieu_Nguyen <rione94@gmail.com>
2017-03-27 16:30:05 +0700

    add Homes controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  	def hello
-    	render 'static_pages/home'
-  	end
+    
+  def hello
+    render 'static_pages/home'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'homes/index'
+
   get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   	get  '/signup',  to: 'users#new'
-	root 'application#hello'  
+	root 'homes#index'  
 end
