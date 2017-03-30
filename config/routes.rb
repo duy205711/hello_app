56728a590275c318a6ab5d534a2b5a996e406013
lieu <rione94@gmail.com>
2017-03-30 18:23:49 +0700

    chap 14

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :home, only: [:index]
   resources :microposts, only: %i(create destroy)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
