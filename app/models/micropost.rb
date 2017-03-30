b4d3a5a336e4c2b4d8ea60ad8ddf0796951a91be
lieu <rione94@gmail.com>
2017-03-30 15:27:41 +0700

    fix bug

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,9 +1,11 @@
+require 'carrierwave/orm/activerecord'
+
 class Micropost < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   default_scope -> { order(created_at: :desc) }
-  mount_uploader :picture, PictureUploader
+  mount_uploader :picture, ::PictureUploader
   validate :picture_size
 
   private
@@ -11,6 +13,5 @@ class Micropost < ApplicationRecord
   def picture_size
     return unless picture.size > 5.megabytes
     errors.add(:picture, 'should be less than 5MB')
-
   end
 end
